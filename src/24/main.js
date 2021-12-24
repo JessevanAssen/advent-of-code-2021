bf6f205996855cbcfbbc5f733c58f3ca5813a5cf
Jesse van Assen <jesse.v.assen@gmail.com>
2021-12-24 05:59:34 +0100

    Start

---
+++ src/24/main.js
@@ -0,0 +1,11 @@
+import { readBlocksFromStdin } from '../lib/index.js';
+
+function parseInput() {
+
+}
+
+
+const input = parseInput();
+
+console.log('Part 1', undefined);
+console.log('Part 2', undefined);
